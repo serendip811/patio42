507227fc1579fd0c2b006d0d49c738c54355cac9
serendip <serendip@serendips-MacBook-Pro.local>
2016-05-05 12:59:00 +0900

    css, js refresh

--- views/franchise.php
+++ views/franchise.php
@@ -3,11 +3,11 @@ use Wandu\Template\Syntax\Template;
 
 Template::setLayout('layout/master', [
     'cssFiles' => [
-        '/static/css/franchise.css'
+        '/static/css/franchise.css?20160505'
     ],
     'jsFiles' => [
-        '/static/js/popup.js',
-        '/static/js/franchise.js'
+        '/static/js/popup.js?20160505',
+        '/static/js/franchise.js?20160505'
     ]
 ]);
 ?>
--- views/index.php
+++ views/index.php
@@ -3,15 +3,15 @@ use Wandu\Template\Syntax\Template;
 
 Template::setLayout('layout/master', [
     'cssFiles' => [
-        '/static/css/index.css'
+        '/static/css/index.css?20160505'
     ],
     'jsFiles' => [
         '/static/vendor/isotope/dist/isotope.pkgd.min.js',
         '/static/vendor/jquery.simpleslider/jquery.simpleslider.min.js',
-        '/static/js/popup.js',
+        '/static/js/popup.js?20160505',
         'http://openapi.map.naver.com/openapi/naverMap.naver?ver=2.0&key=31e4d5fd3ada24ea1057920a526149fb',
         '/static/js/map.js',
-        '/static/js/index.js'
+        '/static/js/index.js?20160505'
     ]
 ]);
 ?>
